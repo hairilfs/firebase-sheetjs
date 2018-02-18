041a8dca027c0753224d2cbf2ca4297bf548a521
hairilfs <hairilfiqri@gmail.com>
2018-02-18 08:46:24 +0700

    download xlsx ok

--- stock_taking/script.js
+++ stock_taking/script.js
@@ -3,22 +3,48 @@ $(function() {
 		$(this).addClass('button-primary').siblings().removeClass('button-primary');
 
 		if ($(this).attr('id') == 'upload_excel') {
-			$('#table_firebase').hide();
+			$('.in-row-firebase').hide();
 			$('#row_file').show();
 		} else {
 			$('#row_file').hide();
 			$('#table_firebase').show();
+			if (_datalist) {
+				$('#download_excel').show();
+			}
 
 		}
 	});
 
 
 	$('#upload_firebase').on('click', function(e){
-
 		var sheet1 = Object.keys(excel_data)[0]; // ambil sheet1/Sheet1
 		var to_firebase = excel_data[sheet1];
 
+		$(this).html('Uploading...');
 		writeUserData(to_firebase);
+		$(this).html('Upload to firebase');
+	});
+
+	$('#download_excel').on('click', function(e){
+		if (_datalist) {
+			console.log('kemari')
+			
+			var filename = _now+"_download.xlsx";
+			var wb = XLSX.utils.book_new();
+			var ws = XLSX.utils.json_to_sheet( _datalist, {header: Object.keys(_datalist[0]), skipHeader: false});
+			var ws_name = "Sheet1";	
+			 
+			/* add worksheet to workbook */
+			XLSX.utils.book_append_sheet(wb, ws, ws_name);
+
+			/* write workbook */
+			XLSX.writeFile(wb, filename);
+
+			console.log(ws)
+			console.log(_datalist)
+			console.log(Object.keys(_datalist[0]))
+
+		}
 	});
 	
 });
@@ -110,29 +136,22 @@ firebase.initializeApp(config);
 
 // Get a reference to the database service
 var _database 	= firebase.database();
-var _datalist	= {};
-var _dir 		= 'stock_taking';
 var _now 		= moment().format("YYYY-MM-DD");
+var _dir 		= 'stock_taking';
 var _table 		= $('#table_firebase');
-var _table_head	= [];
+var _datalist 	= {};
 
 function writeUserData(obj) {
 
-	_table_head = [];
+	var table_head = [];
 
 	obj.forEach(function(item, index) {
 
 		if (index == 0) { 	// ambil headernya
 
-			var thead = '<tr>';
-
 			item.forEach(function(item, index){
-				_table_head.push(item);
-				thead += '<th>'+item+'</th>';
+				table_head.push(item);
 			});
-
-			thead += '</tr>';
-			_table.find('thead').html(thead);
 
 			return;
 
@@ -141,7 +160,7 @@ function writeUserData(obj) {
 			var pairs = [];
 
 			item.forEach(function(item, index){
-				var arr = new Array(_table_head[index], item);
+				var arr = new Array(table_head[index], item);
 				pairs.push(arr);
 			});
 
@@ -158,24 +177,41 @@ function writeUserData(obj) {
 }
 
 _database.ref(_dir +'/'+ _now).on('value', function(snapshot) {
-	_datalist = {};
+
+  	_datalist = {};
   	_datalist = snapshot.val();
   	// window.isi = _datalist;
 
   	if (_datalist) {
-	  	var tbody = '';
+  		var thead_data = [];
+	  	var tbody = thead = '';
 
 	  	_datalist.forEach(function(item, index) {
+
+	  		if (index == 0) {
+	  			thead = '<tr>';
+
+	  			Object.keys(item).forEach(function(item, index){
+	  				thead_data.push(item);
+					thead += '<th>'+item+'</th>';
+	  			});
+				thead += '</tr>';
+				_table.find('thead').html(thead);
+	  		}
+
 	  		tbody += '<tr>';
 
-	  		_table_head.forEach(function(item_head, index_head){
-		  		tbody += 	'<td>'+item[item_head]+'</td>';
+	  		thead_data.forEach(function(item_key, index_key){
+		  		tbody += '<td>'+item[item_key]+'</td>';
 	  		});
 
 	  		tbody += '</tr>';
 	  	});
 
-	  	_table.find('tbody').html(tbody);
+	  	_table.find('tbody').html(tbody);	  	
+
+	  	console.log('Data has been received!')
+
   	}
 
 });
